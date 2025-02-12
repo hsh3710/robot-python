92a605e9e2bab5e23a78e3edc179945dcdf2986e
hsh3710 <86577645+hsh3710@users.noreply.github.com>
2025-02-12 10:49:22 +0900

    369 게임 완성
    컴퓨터와 사용자가 369게임 진행
    - 3, 6, 9 인식 함수
    - 3, 6, 9에 짝 인식 함수
    - 게임시작 함수
    - while문으로 반복문 작성
    - 'q' 입력시 종료
    - 틀렸을 시 종료

--- test.py
+++ test.py
@@ -1,14 +1,43 @@
 import random
+import time
 
-# 3 6 9 게임
-# 사용자가 숫자를 입력하면 3 6 9 게임을 하는 프로그램
-# 3 6 9 게임은 3의 배수를 외치면 박수를 치는 게임
+def contains_369(number):
+    return any(digit in str(number) for digit in ['3', '6', '9'])
 
-input_number = int(input("숫자를 입력하세요: "))    
+def get_correct_response(number):
+    if contains_369(number):
+        return "짝"
+    return str(number)
 
-for i in range(1, input_number + 1):
-    if i % 3 == 0:
-        print("박수")
-    else:
-        print(i)
+def play_369_game():
+    print("369 게임을 시작합니다!")
+    print("3, 6, 9가 들어간 숫자는 '짝'을 입력하세요.")
+    print("게임을 종료하려면 'q'를 입력하세요.")
+    
+    current_number = 1
+    
+    while True:
+        # 사용자 차례
+        print(f"\n현재 숫자: {current_number}")
+        correct_answer = get_correct_response(current_number)
+        user_input = input("당신의 차례: ")
+        
+        if user_input.lower() == 'q':
+            print("게임을 종료합니다.")
+            break
+            
+        if user_input != correct_answer:
+            print(f"틀렸습니다! 정답은 {correct_answer}입니다.")
+            break
+            
+        current_number += 1
+        
+        # 컴퓨터 차례
+        computer_answer = get_correct_response(current_number)
+        print(f"컴퓨터: {computer_answer}")
+        time.sleep(1)  # 컴퓨터의 응답을 보기 쉽게 약간의 딜레이를 줍니다
+        
+        current_number += 1
 
+if __name__ == "__main__":
+    play_369_game()
