e51187cb344a3eb16f8029f8e3871f1455893cce
hsh3710 <86577645+hsh3710@users.noreply.github.com>
2025-02-12 11:48:28 +0900

    1.4 정답 인식 함수
    - 답한 값이 맞는 정답지 함수 생성

--- 369game.py
+++ 369game.py
@@ -8,12 +8,17 @@ def play_369_game():
 def contains_369(number):
     return any(n_list in str(number) for n_list in ['3', '6', '9'])
 
+# 정답지 함수
+def get_correct_response(number):
+    if contains_369(number):
+        return "짝"
+    else:
+        return str(number)
+
+# 사용자 입력받아 1 부터 시작
+
 # 다음 컴퓨터 진행
 
 # 3 6 9에 숫자에 짝 인식하는 함수 생성
-
-# 사용자 컴퓨터 결과 판정 함수 생성
-
-# 판정 결과에따라 진행/종료 실행
 
 # 게임 진행 중 'e' 입력시 프로그램 종료
